9d5101722c0c1348bd5d350c50f119fdd34b0bb7
lucasMarquesDevTi <lucasmarquesdevti@gmail.com>
2024-12-26 00:29:41 -0300

    funçãoEditar

--- editorTextoBalt/Program.cs
+++ editorTextoBalt/Program.cs
@@ -14,7 +14,7 @@ namespace EditorTexto
             Console.Clear();
             Console.WriteLine("O que você deseja fazer?");
             Console.WriteLine("1- Abrir");
-            Console.WriteLine("2- Criar");
+            Console.WriteLine("2- Editar");
             Console.WriteLine("0- Sair");
 
             int option = int.Parse(Console.ReadLine());
@@ -23,13 +23,25 @@ namespace EditorTexto
             {
                 case 0: System.Environment.Exit(0); break;
                 case 1: Abrir(); break;
-                case 2: Criar(); break;
+                case 2: Editar(); break;
                 default: Menu(); break;
             }
         }
 
         static void Abrir() { }
 
-        static void Criar() { }
+        static void Editar() 
+        {
+            Console.Clear();
+            Console.WriteLine("Digite seu texto abaixo (ESC para sair)");
+            Console.WriteLine("---------------------------------");
+
+            string texto = "";
+
+            while (Console.ReadKey().Key != ConsoleKey.Escape) 
+            { 
+
+            }
+        }
     }
 }
